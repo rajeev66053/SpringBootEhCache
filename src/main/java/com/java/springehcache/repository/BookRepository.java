a78648e66a3c9882514c68d6bd7521c7f89f7f1c
Rajeev Thapaliya <rajeevthapaliya@gmail.com>
2022-07-08 14:15:50 -0700

    Spring boot Ehcache

---
+++ src/main/java/com/java/springehcache/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.java.springehcache.repository;
+
+import com.java.springehcache.dto.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+
+public interface BookRepository  extends JpaRepository<Book, Long> {
+    @Transactional
+    @Modifying
+    @Query("update Book u set u.name=?2 where u.id=?1")
+    int updateAddress(long id, String name);
+}
